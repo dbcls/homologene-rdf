d054e73d5f9007632905b81e2f271641bd8858fb
Hirokazu Chiba <hchiba1@gmail.com>
2022-09-29 14:30:29 +0900

    Add bin/homologene.py

--- bin/homologene.py
+++ bin/homologene.py
@@ -8,15 +8,18 @@ args = parser.parse_args()
 
 print("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .")
 print("@prefix dct: <http://purl.org/dc/terms/> .")
+print("@prefix obo: <http://purl.obolibrary.org/obo/> .")
+print("@prefix sio: <http://semanticscience.org/resource/> .")
+print("@prefix orth: <http://purl.org/net/orth#> .")
+print("@prefix homologene: <https://identifiers.org/homologene/> .")
+print("@prefix taxonomy: <http://identifiers.org/taxonomy/> .")
 print("@prefix ncbigene: <http://identifiers.org/ncbigene/> .")
 print("@prefix ncbiprotein: <http://identifiers.org/ncbiprotein/> .")
-print("@prefix homologene: <https://ncbi.nlm.nih.gov/homologene/> .")
-print("@prefix taxid: <http://identifiers.org/taxonomy/> .")
-print("@prefix orth: <http://purl.org/net/orth#> .")
 print()
 print('<https://ncbi.nlm.nih.gov/homologene/>')
 print('    a orth:OrthologyDataset ;')
-print('    dct:title "HomoloGene" .')
+print('    dct:title "HomoloGene Release 68" ;')
+print('    dct:license <https://www.ncbi.nlm.nih.gov/home/about/policies/> .')
 print()
 
 gene_info = {}
@@ -26,8 +29,8 @@ def print_gene_info(gene_id):
     
     print(f'ncbigene:{gene_id} a orth:Gene ;')
     print(f'    rdfs:label "{symbol}" ;')
-    print(f'    orth:taxon taxid:{taxid} ;')
-    print(f'    orth:protein ncbiprotein:{refseq} .')
+    print(f'    obo:RO_0002162 taxonomy:{taxid} ; # in taxon')
+    print(f'    sio:SIO_010078 ncbiprotein:{refseq} . # encodes')
     print()
 
 def print_group(grp_id, genes):
