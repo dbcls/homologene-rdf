8d16c4a3ffe75dc3c561cdd7a5a7f386cc42f9c8
Hirokazu Chiba <hchiba1@gmail.com>
2023-05-17 16:28:28 +0900

    Refactor bin/homologene_xml_to_ttl.py and fix prefix for homologene

--- bin/homologene_xml_to_ttl.py
+++ bin/homologene_xml_to_ttl.py
@@ -2,29 +2,51 @@
 import argparse
 import xml.etree.ElementTree as ET
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('xml', help='HomoloGene XML file')
-args = parser.parse_args()
-
-tree = ET.parse(args.xml)
-root = tree.getroot()
-    
 def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('xml', help='HomoloGene XML file')
+    args = parser.parse_args()
+    
+    tree = ET.parse(args.xml)
+    root = tree.getroot()
+    
     print_header()
     for entries in root:
         for entry in entries:
-            hg_id =  entry.find('HG-Entry_hg-id').text
-            caption = entry.find('HG-Entry_caption').text
-            print(f"homologene:{hg_id} a orth:OrthologsCluster ;")
-            print('    orth:inDataset <https://ncbi.nlm.nih.gov/homologene/> ;')
-            print(f'    dct:identifier {hg_id} ;')
-            print(f'    rdfs:label "{caption}" ;')
-            # print(entry.find('HG-Entry_version').text)
-            # print(entry.find('HG-Entry_taxid').text)
-            gene_ids = get_genes(entry.find('HG-Entry_genes'))
-            print(" ;\n".join(gene_ids) + " .\n")
-            print_genes(entry.find('HG-Entry_genes'))
+            print_entry(entry)
 
+def print_header():
+    print('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .')
+    print('@prefix dct: <http://purl.org/dc/terms/> .')
+    print('@prefix obo: <http://purl.obolibrary.org/obo/> .')
+    print('@prefix sio: <http://semanticscience.org/resource/> .')
+    print('@prefix orth: <http://purl.org/net/orth#> .')
+    print('@prefix homologene: <http://identifiers.org/homologene/> .')
+    print('@prefix taxonomy: <http://identifiers.org/taxonomy/> .')
+    print('@prefix ncbigene: <http://identifiers.org/ncbigene/> .')
+    print('@prefix ncbiprotein: <http://identifiers.org/ncbiprotein/> .')
+    print()
+    print('<https://ncbi.nlm.nih.gov/homologene/>')
+    print('    a orth:OrthologyDataset ;')
+    print('    dct:title "HomoloGene Release 68" ;')
+    print('    dct:license <https://www.ncbi.nlm.nih.gov/home/about/policies/> .')
+    print()
+
+def print_entry(entry):
+    hg_id =  entry.find('HG-Entry_hg-id').text
+    caption = entry.find('HG-Entry_caption').text
+    gene_ids = get_genes(entry.find('HG-Entry_genes'))
+    # version = entry.find('HG-Entry_version').text
+    # taxid = entry.find('HG-Entry_taxid').text
+    
+    print(f"homologene:{hg_id} a orth:OrthologsCluster ;")
+    print('    orth:inDataset <https://ncbi.nlm.nih.gov/homologene/> ;')
+    print(f'    dct:identifier {hg_id} ;')
+    print(f'    rdfs:label "{caption}" ;')
+    print(" ;\n".join(gene_ids) + " .\n")
+    
+    print_genes(entry.find('HG-Entry_genes'))
+    
 def get_genes(genes):
     gene_ids = []
     for gene in genes:
@@ -43,23 +65,6 @@ def print_genes(genes):
         print(f'    obo:RO_0002162 taxonomy:{taxid} ; # in taxon')
         print(f'    sio:SIO_010078 ncbiprotein:{prot} . # encodes')
         print()
-
-def print_header():
-    print('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .')
-    print('@prefix dct: <http://purl.org/dc/terms/> .')
-    print('@prefix obo: <http://purl.obolibrary.org/obo/> .')
-    print('@prefix sio: <http://semanticscience.org/resource/> .')
-    print('@prefix orth: <http://purl.org/net/orth#> .')
-    print('@prefix homologene: <https://identifiers.org/homologene/> .')
-    print('@prefix taxonomy: <http://identifiers.org/taxonomy/> .')
-    print('@prefix ncbigene: <http://identifiers.org/ncbigene/> .')
-    print('@prefix ncbiprotein: <http://identifiers.org/ncbiprotein/> .')
-    print()
-    print('<https://ncbi.nlm.nih.gov/homologene/>')
-    print('    a orth:OrthologyDataset ;')
-    print('    dct:title "HomoloGene Release 68" ;')
-    print('    dct:license <https://www.ncbi.nlm.nih.gov/home/about/policies/> .')
-    print()
 
 if __name__ == '__main__':
     main()
